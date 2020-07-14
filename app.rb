df5501a7361d780f15b8453b8d6e06ef83f3954f
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-07-13 20:11:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require 'sinatra/base'
 
+#really unclear why my solution didn't work!!!
+
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/newteam' do
+        @name = params[:name]
+        @coach = params[:coach]
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @sf = params[:sf]
+        @pf = params[:pf]
+        @c = params[:c]
+        erb :team
+    end
 
 end
+
